1615769d1906ddd4e146fc74a7ffd9ed1954bb7a
Allen <allen3325940072@gmail.com>
2021-03-29 13:51:57 +0800

    feat: check illegal input in sign up page done

--- frontend/WAF/src/sample/CheckSignUp.java
+++ frontend/WAF/src/sample/CheckSignUp.java
@@ -1,28 +1,71 @@
 package sample;
+import java.nio.charset.IllegalCharsetNameException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CheckSignUp
 {
     //data field
-    private static final String regexMail = "^(.+)@(.+)$";
-    private static final String regexPassword = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,20}$";
+    private static final String REGEXMAIL = "^(.+)@(.+)$";
+    private static final char[] ILLEGALCHAR = {'\'', '<', '>','\\','`'};
+    private static final String REGEXPASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,20}$";
+    private static final int LEN = 5;
     private String userPassWord;
     private String userMail;
+    private String userID;
+    private String userName;
     private Pattern pattern;
     private Matcher matcher;
 
-    CheckSignUp(String passWord,String mail){
+    //constructor
+    CheckSignUp(String passWord,String mail,String ID,String name){
         this.userPassWord = passWord;
         this.userMail = mail;
+        this.userID = ID;
+        this.userName = name;
+    }
+
+    //method
+    public boolean checkName()
+    {
+        for(int i = 0;i<LEN;i++)
+        {
+            if(this.userName.indexOf(ILLEGALCHAR[i]) != -1)
+            {
+                System.out.println("Illegal char found!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkID()
+    {
+        for(int i = 0;i<LEN;i++)
+        {
+            if(this.userID.indexOf(ILLEGALCHAR[i]) != -1)
+            {
+                System.out.println("Illegal char found!");
+                return false;
+            }
+        }
+        return true;
     }
 
     public boolean checkPassWord()
     {
         try
         {
-            //checkPassWord
-            this.pattern = Pattern.compile(regexPassword);
+            for(int i = 0;i<LEN;i++)
+            {
+                if(this.userPassWord.indexOf(ILLEGALCHAR[i]) != -1)
+                {
+                    System.out.println("Illegal char found!");
+                    return false;
+                }
+            }
+
+            this.pattern = Pattern.compile(REGEXPASSWORD);
             this.matcher = this.pattern.matcher(this.userPassWord);
             if(this.matcher.matches())
             {
@@ -39,8 +82,16 @@ public class CheckSignUp
     {
         try
         {
-            //checkemail
-            this.pattern = Pattern.compile(regexMail);
+            for(int i = 0;i<LEN;i++)
+            {
+                if(this.userMail.indexOf(ILLEGALCHAR[i]) != -1)
+                {
+                    System.out.println("Illegal char found!");
+                    return false;
+                }
+            }
+
+            this.pattern = Pattern.compile(REGEXMAIL);
             this.matcher = this.pattern.matcher(this.userMail);
             if(this.matcher.matches())
             {
--- frontend/WAF/src/sample/SignUpController.java
+++ frontend/WAF/src/sample/SignUpController.java
@@ -8,23 +8,32 @@ import javafx.scene.control.TextField;
 public class SignUpController
 {
     public TextField userID,userPWConfirm,userMail,userPassword,userName;
-    public Label test,ckName,ckPassword,ckMail,ckID,ckPWConfirm;
+    public Label ckName,ckPassword,ckMail,ckID,ckPWConfirm;
 
     public void checkInput(ActionEvent actionEvent)
     {
         String password = userPassword.getText();
         String mail = userMail.getText();
-        CheckSignUp checkUser = new CheckSignUp(password,mail);
+        String ID = userID.getText();
+        String name = userName.getText();
+        CheckSignUp checkUser = new CheckSignUp(password,mail,ID,name);
+        //check illegal input
+        if(checkUser.checkName())
+            ckName.setText("OK");
+        else
+            ckName.setText("WRONG");
+        if(checkUser.checkID())
+            ckID.setText("OK");
+        else
+            ckID.setText("WRONG");
         if(checkUser.checkPassWord())
             ckPassword.setText("OK");
         else
             ckPassword.setText("WRONG");
-
         if(password.equals(userPWConfirm.getText()))
             ckPWConfirm.setText("OK");
         else
             ckPWConfirm.setText("WRONG");
-
         if(checkUser.checkMail())
             ckMail.setText("OK");
         else
